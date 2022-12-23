e772410cc9d0289cf347ba4dfda47da83d4ff075
Ahmed Sabry <ahmedmsabry2000@gmail.com>
2022-12-23 10:46:56 -0800

    fixed environment setup and dependencies

--- cavity-service/model/main.py
+++ cavity-service/model/main.py
@@ -1,35 +1,13 @@
-import base64
-import io
-
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
-from CavityModel import CavityModel
+from flask_restful import Api
 
-from PIL import Image
+from model.CavityServer import CavityServer
 
 app = Flask(__name__)
 api = Api(app)
 
 ENDPOINT = "/cavity"
 PORT = 5003
-
-cavity_model = CavityModel()
-
-image_arg = "image"
-
-cavity_get_args = reqparse.RequestParser()
-cavity_get_args.add_argument(image_arg)
-
-
-class CavityServer(Resource):
-    def get(self):
-        args = cavity_get_args.parse_args()
-        img: str = args[image_arg]
-        imgdata = base64.b64decode(img)
-        img = Image.open(io.BytesIO(imgdata))
-        is_cavity = cavity_model.get_is_cavity(img)
-        return is_cavity, 200
-
 
 api.add_resource(CavityServer, ENDPOINT)
 
